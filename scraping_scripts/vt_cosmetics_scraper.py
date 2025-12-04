2493584642b878413d822ddb65640ca8efad85d8
dibiti <dora.c.goncalves@hotmail.com>
2025-12-04 18:58:36 +0100

    Extraction of the link products of the first page

--- scraping_scripts/vt_cosmetics_scraper.py
+++ scraping_scripts/vt_cosmetics_scraper.py
@@ -6,6 +6,7 @@ from selenium.common.exceptions import WebDriverException
 from bs4 import BeautifulSoup
 import re
 import json
+from typing import List
 
 BASE_URL = "https://globalvt-cosmetics.com"
 NEW_URL = BASE_URL + "/collections/all"
@@ -47,6 +48,41 @@ def fetch_page(url):
 
     return html_content
 
+def extract_product_links(html_content: str | None) -> List[str]:
+    if not html_content:
+        return []
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+    product_links: List[str] = [] #Using Type Hinting
+    
+    EXCLUSION_KEYWORDS = ["bundle", "kit", "collection"]
+    link_tags = soup.find_all('a', class_='prgrid-name-style') 
+
+    for tag in link_tags:
+        relative_link = tag.get('href') 
+        
+        if relative_link and relative_link.startswith('/products/'): 
+            clean_link = relative_link.split('?')[0]
+            lower_link = clean_link.lower()
+            should_exclude = any(keyword in lower_link for keyword in EXCLUSION_KEYWORDS)
+            
+            if should_exclude: 
+                # print(f"   -> SKIPPING (Excluded): {relative_link}")
+                continue
+                
+            full_url = BASE_URL + clean_link
+            product_links.append(full_url)
+            
+    return list(set(product_links))
 
 if __name__ == "__main__":
-    fetch_page(NEW_URL)
+    html_source = fetch_page(NEW_URL)
+    
+    if html_source:
+        all_product_urls = extract_product_links(html_source)
+        if all_product_urls:
+            print("\n Example of Extracted Links:")
+            for url in all_product_urls:
+                print(f" - {url}")
+        else:
+            print("\nNo product links were extracted. Check the HTML structure.")    
